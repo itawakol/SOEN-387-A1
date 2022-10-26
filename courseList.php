b5cc2d8ae6aea0f4518239919abfe5d5fdb34913
itawakol <57954077+itawakol@users.noreply.github.com>
2022-10-26 13:57:15 -0400

    Update courseList.php

--- courseList.php
+++ courseList.php
@@ -1,53 +1,49 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>List of courses</title>
-</head>
+    <head>
+        <title>List of courses</title>
+    </head>
 <body>
-<table>
-    <tr>
-        <th>student name</th>
-        <th>course1</th>
-        <th>course2</th>
-        <th>course3</th>
-        <th>course4</th>
-        <th>course5</th>
+    <table>
+        <tr>
+            <th>student name</th>
+            <th>course1</th>
+            <th>course2</th>
+            <th>course3</th>
+            <th>course4</th>
+            <th>course5</th>
+</tr>
+<?php
+$hostName = "localhost";
+$userName = "root";
+$password = "";
+$databaseName = "assignment 1";
+ $conn = new mysqli($hostName, $userName, $password, $databaseName);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$sid = $_POST["studentID"];
+$query = "SELECT studentID, courseCode1, courseCode2, courseCode3, courseCode4, courseCode5 FROM enrolledin WHERE studentID=$sid";
+$result = $conn->query($query);
+
+if ($result->num_rows > 0) {
+    while($data = $result->fetch_assoc()) {
+?>
+<tr>
+    <td><?php echo $data['studentID']; ?></td>
+    <td><?php echo $data['courseCode1']; ?></td>
+    <td><?php echo $data['courseCode2']; ?></td>
+    <td><?php echo $data['courseCode3']; ?></td>
+    <td><?php echo $data['courseCode4']; ?></td>
+    <td><?php echo $data['courseCode5']; ?></td>
     </tr>
     <?php
-    extract( $_POST );
-
-    $studentID = $_POST["studentID"];
-
-    if ( !( $database = mysqli_connect( "localhost",
-        "root", "") ) ) {
-        die( "Could not connect to database </body></html>" );
-    };
-
-
-    if ( !mysqli_select_db( $database ,"assignment1" ) ) {
-        die( "Could not open Online Registration database </body></html>" );
-    };
-    
-    $query = "SELECT studentID, courseCode1, courseCode2, courseCode3, courseCode4, courseCode5 FROM enrolledin WHERE studentID=$sid";
-    $result = $conn->query($query);
-
-    if ($result->num_rows > 0) {
-        while($data = $result->fetch_assoc()) {
-            ?>
-            <tr>
-                <td><?php echo $data['studentID']; ?></td>
-                <td><?php echo $data['courseCode1']; ?></td>
-                <td><?php echo $data['courseCode2']; ?></td>
-                <td><?php echo $data['courseCode3']; ?></td>
-                <td><?php echo $data['courseCode4']; ?></td>
-                <td><?php echo $data['courseCode5']; ?></td>
-            </tr>
-            <?php
-        }}else { ?>
+    }}else { ?>
         <tr>
-            <td colspan="8">No data found</td>
+         <td colspan="8">No data found</td>
         </tr>
-    <?php } ?>
-</table>
+     <?php } ?>
+    </table>
 <body>
     
