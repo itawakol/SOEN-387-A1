f1973ec4938ca749f5a7db4dbc3f4d5b0dd03f3d
itawakol <57954077+itawakol@users.noreply.github.com>
2022-11-12 18:50:16 -0500

    Update studentServer2.php

--- studentServer2.php
+++ studentServer2.php
@@ -1,9 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title></title>
+    <title>New Student Registration</title>
 </head>
 <body>
+<a href="registrationPage.html">
+            <button type="submit">Press here to go to login page</button>
+</a>
+<a href="newStudentRegistration.html">
+            <button type="submit">Press here to enter a new ID</button>
+</a>
+        
 
 <?php
 
@@ -25,19 +32,31 @@ if ( !( $database = mysqli_connect( "localhost",
     };
 
 
-if ( !mysqli_select_db( $database ,"assignment1" ) ) {
+if ( !mysqli_select_db( $database ,"assignment 1" ) ) {
     die( "Could not open Online Registration database </body></html>" );
 };
+	
 $query="INSERT INTO student (studentID,firstName,lastName,address,email,phoneNumber,dateOfBirth)
 				 VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+
 $stmt = mysqli_stmt_init($database);
+
+
+$result = mysqli_query($database, "SELECT * FROM student WHERE studentID='$id'");
+$num_rows = mysqli_num_rows($result);
+if($num_rows) {
+    trigger_error('student id already exists');
+}
+	
 if (!mysqli_stmt_prepare($stmt, $query)){
     die(mysqli_error($database));
 }
+	
 mysqli_stmt_bind_param($stmt, "issssii", $id, $firstName, $lastName,
                          $address, $email, $phoneNumber, $dob);
 mysqli_stmt_execute($stmt);
-echo "record has been saved";
+echo "New Student record has been saved";
 
 
 
