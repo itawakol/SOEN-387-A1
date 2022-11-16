619337cfdfda45ee1963709f55bb5c77d1a8adda
rabiarana7 <56521751+rabiarana7@users.noreply.github.com>
2022-11-16 15:59:47 -0500

    Update studentList.php

--- studentList.php
+++ studentList.php
@@ -4,11 +4,15 @@
         <title>List of students</title>
     </head>
 <body>
+
+
     <table>
         <tr>
             <th>student ID</th>
 </tr>
 <?php
+extract( $_POST );
+
 $hostName = "localhost";
 $userName = "root";
 $password = "";
@@ -18,15 +22,22 @@ $databaseName = "assignment1";
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
+
+
 $cid = $_POST["courseCode"];
 $query = "SELECT studentID FROM enrolledin WHERE courseCode1='$cid' or courseCode2='$cid' or courseCode3='$cid' or courseCode4='$cid' or courseCode5='$cid'";
 $result = $conn->query($query);
+
+echo "List of students registered in $cid :";
+
+
 
 if ($result->num_rows > 0) {
     while($data = $result->fetch_assoc()) {
 ?>
 <tr>
-    <td><?php echo $data['studentID']; ?></td>
+    <td><?php
+              echo $data['studentID']; ?></td>
     </tr>
     <?php
     }}else { ?>
@@ -35,5 +46,14 @@ if ($result->num_rows > 0) {
         </tr>
      <?php } ?>
     </table>
-<body>
+<br>
+    <br>
+
+    <a href="adminHub.php">
+        <button type="submit">Press here to go back to your hub </button>
+    </a>
+    <a href="registrationPage.php">
+        <button type="submit">Press here to logout </button>
+    </a>
     
+</body> </HTML>
